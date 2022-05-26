1e45d4f7959a348c4be960aaeb2dcb9425c52f47
Morlotllp <estatic_fear@abv.bg>
2022-05-26 23:22:53 +0300

    Added Folder
    Exercise 09 added

---
+++ SoftUni_Fundamentals/DataTypesandVars/DataTypesAndVars/CharsToString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string finalString = null;
+
+            for (int i = 0; i < 3; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+
+                finalString += input;
+            }
+            Console.WriteLine(finalString);
+        }
+    }
+}
