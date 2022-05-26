cdf5e5dab308362049bc14c6b9541d0ee68fe748
Morlotllp <estatic_fear@abv.bg>
2022-05-26 19:44:07 +0300

    ExactSumOfRealNumbers folder
    Folder with ex 03 added

---
+++ SoftUni_Fundamentals/DataTypesandVars/DataTypesAndVars/ExactSumOfREalNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace SumRealNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCount = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 0; i < numCount; i++)
+            {
+                decimal n = decimal.Parse(Console.ReadLine());
+                sum += n;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
