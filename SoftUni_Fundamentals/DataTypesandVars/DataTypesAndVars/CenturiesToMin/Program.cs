e9d0e44ed554281aa7edcc6942a932e9cf0c9344
Morlotllp <estatic_fear@abv.bg>
2022-05-26 21:01:19 +0300

    Folder CenturiesToMinutes added
    Added ex 4

---
+++ SoftUni_Fundamentals/DataTypesandVars/DataTypesAndVars/CenturiesToMin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CenturiesToMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = byte.Parse(Console.ReadLine());
+            ushort years = 0;
+            uint days = 0;
+            ulong hours = 0;
+            ulong minutes = 0;
+
+            //years = centuries * 100
+            //days = years * 365.2422 
+            //minutes = (days * 24) * 60
+
+            years = (ushort)(centuries * 100);
+            days = (uint)(years * 365.2422);
+            hours = days * 24;
+            minutes = hours * 60;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} = {minutes} minutes");
+        }
+    }
+}
