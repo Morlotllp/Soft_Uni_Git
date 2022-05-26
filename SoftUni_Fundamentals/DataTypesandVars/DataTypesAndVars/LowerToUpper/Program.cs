9b9b612ecaa2bad78834e09d29d8a09b766ea8a2
Morlotllp <estatic_fear@abv.bg>
2022-05-26 23:38:27 +0300

    Repo updated
    Added folder LowerToUpper

---
+++ SoftUni_Fundamentals/DataTypesandVars/DataTypesAndVars/LowerToUpper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LowerToUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+            if (Char.IsLower(input))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
