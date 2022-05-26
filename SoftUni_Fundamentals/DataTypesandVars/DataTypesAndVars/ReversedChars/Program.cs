8fe020e926630369afa70ae07665e9a86f312358
Morlotllp <estatic_fear@abv.bg>
2022-05-26 22:37:25 +0300

    Folder SpecialChars added
    Added exercise

---
+++ SoftUni_Fundamentals/DataTypesandVars/DataTypesAndVars/ReversedChars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] dumpBin = new char[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                char character = char.Parse(Console.ReadLine());
+                dumpBin[i] = character;
+            }
+
+            for (int i = dumpBin.Length; i > 0 ; i--)
+            {
+                Console.Write(dumpBin[i - 1] + " ");
+            }
+        }
+    }
+}
---
+++ SoftUni_Fundamentals/DataTypesandVars/DataTypesAndVars/SpecNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SpecNumbers
+{
+    class Program
+    {
+        static int CalculateDigits(int inputS)
+        {
+            string input = inputS.ToString();
+            int sumDigits = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int digit = Convert.ToInt32(input[i].ToString());
+                sumDigits += digit;
+            }
+            return sumDigits;
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int convertInput = int.Parse(input);
+            bool isSpecial = false;
+
+            for (int i = 1; i <= convertInput; i++)
+            {
+                int currentNum = CalculateDigits(i);
+                if (currentNum  == 5 || currentNum == 7 || currentNum == 11)
+                {
+                    isSpecial = true;
+                }
+                Console.WriteLine($"{i} -> {isSpecial}");
+
+                currentNum = 0;
+                isSpecial = false;
+            }
+            
+        }
+    }
+}
