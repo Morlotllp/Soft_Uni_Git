3413790693f9d3e7497727e4ce04a19b53482682
Morlotllp <estatic_fear@abv.bg>
2022-05-27 23:10:33 +0300

    Added TripplesOfLetters
    Exercise added

---
+++ SoftUni_Fundamentals/DataTypesAndVars_ex/TripplesOfLetters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TripplesOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            //ASCII lowercase letters range 97 - 122
+                     
+            for (int i = 0; i < num; i++)
+            {
+                int asciiIndex1 = i + 97;
+
+                for (int j = 0; j < num; j++)
+                {
+                    int asciiIndex2 = j + 97;
+                    for (int k = 0; k < num; k++)
+                    {
+                        int asciiIndex3 = k + 97;
+                        Console.WriteLine($"{(char)asciiIndex1}{(char)asciiIndex2}{(char)asciiIndex3}");
+                    }
+                }
+            }
+        }
+    }
+}
