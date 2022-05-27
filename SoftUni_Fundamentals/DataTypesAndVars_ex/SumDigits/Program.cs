fa85520e36648267215b0f841c51323ba786ff09
Morlotllp <estatic_fear@abv.bg>
2022-05-27 21:21:33 +0300

    Added content
    Exercise 2 added

---
+++ SoftUni_Fundamentals/DataTypesAndVars_ex/SumDigits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumDigitsProgram
+{
+    class Program
+    {
+		public static int SumDigits(int inputInt)
+		{
+			string inputString = inputInt.ToString();
+			int sumDigits = 0;
+
+			for (int i = 0; i < inputString.Length; i++)
+			{
+				int currentDigit = int.Parse(inputString[i].ToString());
+				sumDigits += currentDigit;
+			}
+			return sumDigits;
+		}
+		static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(SumDigits(input));
+        }
+    }
+}
