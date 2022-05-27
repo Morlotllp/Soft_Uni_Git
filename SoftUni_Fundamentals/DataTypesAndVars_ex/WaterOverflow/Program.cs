1814a7aaff03a80bdf3f81fd521cdcb834061df4
Morlotllp <estatic_fear@abv.bg>
2022-05-27 23:58:26 +0300

    Add file WaterOverflow.csproj
    Exercise added

---
+++ SoftUni_Fundamentals/DataTypesAndVars_ex/WaterOverflow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pourTimes = int.Parse(Console.ReadLine());
+            int tankCapacity = 255;
+            int litresPoured = 0;
+
+            for (int i = 0; i < pourTimes; i++)
+            {
+                int waterQuantity = int.Parse(Console.ReadLine());
+
+                if (waterQuantity > tankCapacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+
+                }
+                else
+                {
+                    litresPoured += waterQuantity;
+                    tankCapacity -= waterQuantity;
+                }
+            }
+            Console.WriteLine(litresPoured);
+        }
+    }
+}
