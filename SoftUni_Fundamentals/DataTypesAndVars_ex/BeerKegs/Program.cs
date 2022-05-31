b882cae5b42e5e88f66ce3917171baee98bf5953
Morlotllp <estatic_fear@abv.bg>
2022-05-31 23:41:27 +0300

    Added project BeerKegs

---
+++ SoftUni_Fundamentals/DataTypesAndVars_ex/BeerKegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kegNumber = int.Parse(Console.ReadLine());
+            double printVol = int.MinValue;
+            string printName = null;
+
+            for (int i = 0; i < kegNumber; i++)
+            {
+                string kegModel = Console.ReadLine();
+                double kegRadius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(kegRadius, 2) * height;
+                
+                if(volume > printVol)
+                {
+                    printVol = volume;
+                    printName = kegModel;
+                }
+            }
+            Console.WriteLine($"{printName}");
+        }
+    }
+}
