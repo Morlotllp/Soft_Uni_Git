2fc68a9ee60a7254531cc531ab4fffb58f358e58
Morlotllp <estatic_fear@abv.bg>
2022-06-01 21:15:17 +0300

    Update Program.cs
    update

--- SoftUni_Fundamentals/DataTypesAndVars_ex/Spces/Program.cs
+++ SoftUni_Fundamentals/DataTypesAndVars_ex/Spces/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
-namespace Spces
+namespace Spices
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int YIELD_DECREASE = 10;
+            const int CONSUMED_BY_WORKERS = 26;
+            const int IS_PROFITABLE = 100;
+
+            int yield = int.Parse(Console.ReadLine());
+            int days = 0;
+            int totlalSpices = 0;
+
+            while (yield >= IS_PROFITABLE)
+            {
+                days++;
+                totlalSpices += yield;
+                totlalSpices -= CONSUMED_BY_WORKERS;
+                yield -= YIELD_DECREASE;
+                if (yield < 100)
+                {
+                    totlalSpices -= CONSUMED_BY_WORKERS;
+                }
+            }
+            Console.WriteLine(days);
+            Console.WriteLine(totlalSpices);
         }
     }
 }
