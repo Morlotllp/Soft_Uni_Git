0bdf506851367516056972339a546e1a69356922
Morlotllp <estatic_fear@abv.bg>
2022-06-01 22:32:25 +0300

    Added Poke_Mon
    Exercise added

---
+++ SoftUni_Fundamentals/DataTypesAndVars_ex/Poke_mon/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //N
+            int pokePower = int.Parse(Console.ReadLine());
+            //M
+            int distance = int.Parse(Console.ReadLine());
+            //Y
+            int exhaustFactor = int.Parse(Console.ReadLine());
+            //pokes count
+            int pokeCount = 0;
+            //calculate 50% of poke power
+            double fiftyPercent = pokePower / 2.0;
+
+            while (pokePower >= distance)
+            {
+                if (pokePower == fiftyPercent && exhaustFactor > 0)
+                {
+                    pokePower /= exhaustFactor;
+                }
+                else
+                {
+                    pokePower -= distance;
+                    pokeCount++;
+                }
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(pokeCount);
+        }
+    }
+}
