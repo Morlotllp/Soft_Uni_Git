42ceab3c51c1c94361c935fabf7c75594086a44d
Morlotllp <estatic_fear@abv.bg>
2022-06-08 19:09:30 +0300

    Arrays1 added
    Exercise 1 added

---
+++ SoftUni_Fundamentals/Arrays_Lab/Arrays_Lab/Arrays_Lab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = {
+                "Monday",
+                "Tuseday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            int num = int.Parse(Console.ReadLine());
+            if (num >= 1 && num <= 7)
+            {
+                Console.WriteLine(days[num-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Day!");
+            }
+        }
+    }
+}
