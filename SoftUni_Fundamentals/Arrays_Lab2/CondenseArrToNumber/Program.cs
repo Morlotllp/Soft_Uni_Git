4a7aa274b3af5e10b74fcafcf5e10a7198a2495c
Morlotllp <estatic_fear@abv.bg>
2022-06-15 19:01:17 +0300

    Problem uploaded
    08. Condense Array to Number

--- SoftUni_Fundamentals/Arrays_Lab2/CondenseArrToNumber/Program.cs
+++ SoftUni_Fundamentals/Arrays_Lab2/CondenseArrToNumber/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
-namespace CondenseArrToNumber
+using System.Linq;
+namespace CondensedNumber
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] myArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int elements = myArray.Length;
+            
+            while (elements > 1)
+            {
+                int sum = 0;
+                
+                int[] condensed = new int[elements - 1];
+
+                for (int i = 1; i < elements; i++)
+                {
+                    sum = myArray[i] + myArray[i - 1];
+                    condensed[i - 1] = sum;
+                }
+                elements--;
+                myArray = condensed;
+            }
+            foreach (int item in myArray)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
