390c90033b755321c80b40ec38a72c3afbccea87
Morlotllp <estatic_fear@abv.bg>
2022-06-15 15:13:51 +0300

    File edited
    File edited

---
+++ SoftUni_Fundamentals/Arrays_Lab2/CondenseArrToNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CondenseArrToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
