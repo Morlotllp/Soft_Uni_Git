a50c95f94fb659d5bba7338efc7ffa4261a7fd22
Morlotllp <estatic_fear@abv.bg>
2022-06-15 19:21:37 +0300

    Folder Arrays_Ex added
    New exercise uploaded

---
+++ SoftUni_Fundamentals/Arrays_Ex/Arrays_Ex/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonNumber = int.Parse(Console.ReadLine());
+            int[] passengers = new int[wagonNumber];
+            int totalPassengers = 0;
+
+            for (int i = 0; i < wagonNumber; i++)
+            {
+                passengers[i] = int.Parse(Console.ReadLine());
+                totalPassengers += passengers[i];
+            }
+
+            foreach (int item in passengers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n" + totalPassengers);
+        }
+    }
+}
